9bf965bcb244fd3e5cc6de5d1af053d8d3cd513a
Tan Kai <6572withspicy@gmail.com>
2025-01-20 23:37:24 +0800

    dev: init env vault

--- pkg/env/src/index.ts
+++ pkg/env/src/index.ts
@@ -1,118 +1,22 @@
 // This file is auto-generated. Do not edit it directly.
 
 export interface BACKEND_BINDING {
-  CLERK_PUBLISHABLE_KEY: string;
-  CLERK_SECRET_KEY: string;
-  CLOUDFLARE_IMAGE_ENDPOINT: string;
-  CLOUDFLARE_IMAGE_TOKEN: string;
-  CLOUDFLARE_IMAGE_AUTH_TOKEN: string;
   DEV_ENV: string;
-  REDIS_URL: string;
-  REDIS_TOKEN: string;
-  STG_DATABASE_URL: string;
-  NEON_STG_DATABASE_URL: string;
-  NEON_PRD_DATABASE_URL: string;
-  DATABASE_URL: string;
-  LOCAL_DATABASE_URL: string;
-  PRD_DATABASE_URL: string;
-  CLOUDFLARE_API_KEY: string;
-  CLOUDFLARE_ACCOUNT_ID: string;
-  ADMIN_USER_ID: string;
-  VERCEL_ORG_ID: string;
-  TURBO_TOKEN: string;
-  VERCEL_FE_PROJECT_ID: string;
-  VITE_PUBLIC_KEY: string;
-  VITE_LINK: string;
-  VITE_BACKEND_URL: string;
-  VITE_IMAGE_URL: string;
-  VITE_IMAGE_URL_TOKEN: string;
   DOTENV_ME: string;
 }
 
 export interface FRONTEND_BINDING {
-  CLERK_PUBLISHABLE_KEY: string;
-  CLERK_SECRET_KEY: string;
-  CLOUDFLARE_IMAGE_ENDPOINT: string;
-  CLOUDFLARE_IMAGE_TOKEN: string;
-  CLOUDFLARE_IMAGE_AUTH_TOKEN: string;
   DEV_ENV: string;
-  REDIS_URL: string;
-  REDIS_TOKEN: string;
-  STG_DATABASE_URL: string;
-  NEON_STG_DATABASE_URL: string;
-  NEON_PRD_DATABASE_URL: string;
-  DATABASE_URL: string;
-  LOCAL_DATABASE_URL: string;
-  PRD_DATABASE_URL: string;
-  CLOUDFLARE_API_KEY: string;
-  CLOUDFLARE_ACCOUNT_ID: string;
-  ADMIN_USER_ID: string;
-  VERCEL_ORG_ID: string;
-  TURBO_TOKEN: string;
-  VERCEL_FE_PROJECT_ID: string;
-  VITE_PUBLIC_KEY: string;
-  VITE_LINK: string;
-  VITE_BACKEND_URL: string;
-  VITE_IMAGE_URL: string;
-  VITE_IMAGE_URL_TOKEN: string;
   DOTENV_ME: string;
 }
 
 export interface PACKAGE_BINDING {
-  CLERK_PUBLISHABLE_KEY: string;
-  CLERK_SECRET_KEY: string;
-  CLOUDFLARE_IMAGE_ENDPOINT: string;
-  CLOUDFLARE_IMAGE_TOKEN: string;
-  CLOUDFLARE_IMAGE_AUTH_TOKEN: string;
   DEV_ENV: string;
-  REDIS_URL: string;
-  REDIS_TOKEN: string;
-  STG_DATABASE_URL: string;
-  NEON_STG_DATABASE_URL: string;
-  NEON_PRD_DATABASE_URL: string;
-  DATABASE_URL: string;
-  LOCAL_DATABASE_URL: string;
-  PRD_DATABASE_URL: string;
-  CLOUDFLARE_API_KEY: string;
-  CLOUDFLARE_ACCOUNT_ID: string;
-  ADMIN_USER_ID: string;
-  VERCEL_ORG_ID: string;
-  TURBO_TOKEN: string;
-  VERCEL_FE_PROJECT_ID: string;
-  VITE_PUBLIC_KEY: string;
-  VITE_LINK: string;
-  VITE_BACKEND_URL: string;
-  VITE_IMAGE_URL: string;
-  VITE_IMAGE_URL_TOKEN: string;
   DOTENV_ME: string;
 }
 
 export interface ENV_BINDING {
-  CLERK_PUBLISHABLE_KEY: string;
-  CLERK_SECRET_KEY: string;
-  CLOUDFLARE_IMAGE_ENDPOINT: string;
-  CLOUDFLARE_IMAGE_TOKEN: string;
-  CLOUDFLARE_IMAGE_AUTH_TOKEN: string;
   DEV_ENV: string;
-  REDIS_URL: string;
-  REDIS_TOKEN: string;
-  STG_DATABASE_URL: string;
-  NEON_STG_DATABASE_URL: string;
-  NEON_PRD_DATABASE_URL: string;
-  DATABASE_URL: string;
-  LOCAL_DATABASE_URL: string;
-  PRD_DATABASE_URL: string;
-  CLOUDFLARE_API_KEY: string;
-  CLOUDFLARE_ACCOUNT_ID: string;
-  ADMIN_USER_ID: string;
-  VERCEL_ORG_ID: string;
-  TURBO_TOKEN: string;
-  VERCEL_FE_PROJECT_ID: string;
-  VITE_PUBLIC_KEY: string;
-  VITE_LINK: string;
-  VITE_BACKEND_URL: string;
-  VITE_IMAGE_URL: string;
-  VITE_IMAGE_URL_TOKEN: string;
   DOTENV_ME: string;
 }
 
